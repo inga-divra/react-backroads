0a8eac35edf67e47096705dbae3ca89f2ee1de09
inga-divra <ingady@gmail.com>
2023-01-29 20:09:30 +0200

    refactoring footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,67 +1,41 @@
 import React from 'react';
+import { pageLinks, socialLinks } from '../data';
 
 const Footer = () => {
   return (
     <footer className='section footer'>
       <ul className='footer-links'>
-        <li>
-          <a href='#home' className='footer-link scroll-link'>
-            home
-          </a>
-        </li>
-        <li>
-          <a href='#about' className='footer-link scroll-link'>
-            about
-          </a>
-        </li>
-        <li>
-          <a href='#services' className='footer-link scroll-link'>
-            services
-          </a>
-        </li>
-        <li>
-          <a href='#featured' className='footer-link scroll-link'>
-            featured
-          </a>
-        </li>
-        <li>
-          <a href='#gallery' className='footer-link scroll-link'>
-            gallery
-          </a>
-        </li>
+        {pageLinks.map((link) => {
+          const { id, href, text } = link;
+          return (
+            <li key={id}>
+              <a href={href} className='footer-link scroll-link'>
+                {text}
+              </a>
+            </li>
+          );
+        })}
       </ul>
       <ul className='footer-icons'>
-        <li>
-          <a
-            href='https://facebook.com/'
-            target='_blank'
-            className='footer-icon'
-          >
-            <i className='fab fa-facebook'></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href='https://twitter.com/'
-            target='_blank'
-            className='footer-icon'
-          >
-            <i className='fab fa-twitter'></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href='https://squarespace.com/'
-            target='_blank'
-            className='footer-icon'
-          >
-            <i className='fab fa-squarespace'></i>
-          </a>
-        </li>
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link;
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target='_blank'
+                rel='noreferrer'
+                className='footer-icon'
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
       </ul>
       <p className='copyright'>
-        copyright &copy; backrouds travel tour company <span id='date'></span>.
-        all rights reserved
+        copyright &copy; backrouds travel tour company{' '}
+        <span id='date'>{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
   );
