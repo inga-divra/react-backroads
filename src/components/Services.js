1d640031d3385468cd8127ac2269893c1f0c5a96
inga-divra <ingady@gmail.com>
2023-01-29 19:26:27 +0200

    refactoring service component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,48 +1,26 @@
 import React from 'react';
 import Title from './Title';
+import { services } from '../data';
 
 const Services = () => {
   return (
     <section className='section services' id='services'>
       <Title title='our' subtitle='services' />
       <div className='section-center services-center'>
-        <article className='service'>
-          <span className='service-icon'>
-            <i className='fas fa-wallet fa-fw'></i>
-          </span>
-          <div className='service-info'>
-            <h4 className='service-title'>saving money</h4>
-            <p className='service-text'>
-              Looking for budget-friendly trips? Get inspired by our selection
-              of the best budget tours that will take you to exciting
-              destinations!
-            </p>
-          </div>
-        </article>
-        <article className='service'>
-          <span className='service-icon'>
-            <i className='fas fa-tree fa-fw'></i>
-          </span>
-          <div className='service-info'>
-            <h4 className='service-title'>endless hiking</h4>
-            <p className='service-text'>
-              Charge the batteries! Pick one of the tours we created based on
-              the general interests of our customers, or create your own trip!
-            </p>
-          </div>
-        </article>
-        <article className='service'>
-          <span className='service-icon'>
-            <i className='fas fa-socks fa-fw'></i>
-          </span>
-          <div className='service-info'>
-            <h4 className='service-title'>amazing comfort</h4>
-            <p className='service-text'>
-              Let's start planning your next vacation! Committed to providing
-              the best value with exceptional service. Travel with comfort!
-            </p>
-          </div>
-        </article>
+        {services.map((service) => {
+          const { id, icon, title, text } = service;
+          return (
+            <article className='service' key={id}>
+              <span className='service-icon'>
+                <i className={icon}></i>
+              </span>
+              <div className='service-info'>
+                <h4 className='service-title'>{title}</h4>
+                <p className='service-text'>{text}</p>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
--- src/data.js
+++ src/data.js
@@ -22,3 +22,24 @@ export const socialLinks = [
     icon: 'fab fa-squarespace',
   },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving money',
+    text: ' Looking for budget-friendly trips? Get inspired by our selection of the best budget tours that will take you to exciting destinations!',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'endless hiking',
+    text: 'Charge the batteries! Pick one of the tours we created based on the general interests of our customers, or create your own trip!',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks fa-fw',
+    title: 'amazing comfort',
+    text: ' Let`s start planning your next vacation! Committed to providing the best value with exceptional service. Travel with comfort!',
+  },
+];
