b57f9764f20ece8cfbd94f5904a24d44fac57126
inga-divra <ingady@gmail.com>
2023-01-29 19:59:26 +0200

    refactoring tours component

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from './images/tour-1.jpg';
+import tour2 from './images/tour-2.jpg';
+import tour3 from './images/tour-3.jpg';
+import tour4 from './images/tour-4.jpg';
+
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
@@ -41,5 +46,48 @@ export const services = [
     icon: 'fas fa-socks fa-fw',
     title: 'amazing comfort',
     text: ' Let`s start planning your next vacation! Committed to providing the best value with exceptional service. Travel with comfort!',
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'august 26th, 2023',
+    title: 'Tibet Adventure',
+    info: 'Neque dolor dignissim fermentum id ut ultricies at egestas egestas. Sit porta imperdiet iaculis venenatis dictum convallis amet. Nullam habitant consectetur accumsan nibh iaculis pellentesque proin mauris bibendum.',
+    location: 'china',
+    duration: 6,
+    cost: '2100',
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'october 1th, 2023',
+    title: 'best of java',
+    info: 'At dolor vestibulum nunc in facilisi. Placerat ultrices arcu sodales eu faucibus ut. A etiam sollicitudin cras neque id tortor.',
+    location: 'indonesia',
+    duration: 11,
+    cost: '1400',
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'september 15th, 2023',
+    title: 'explore hong kong',
+    info: 'Morbi quis sit ultricies duis nullam pretium interdum cras cursus. Id urna varius in nunc. Lacinia nascetur turpis turpis non vestibulum.',
+    location: 'hong kong',
+    duration: 8,
+    cost: '5000',
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'december 5th, 2023',
+    title: 'kenya highlights',
+    info: 'Congue sit nisl sapien mi amet. Sodales aliquam mattis non dictum lobortis sit diam. Ut non ut sed egestas non vulputate viverra tincidunt.',
+    location: 'kenya',
+    duration: 20,
+    cost: '3300',
   },
 ];
